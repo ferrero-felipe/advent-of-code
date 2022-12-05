4082f7387e8bc6ca7464cf85664a11ff25b468c1
Felipe Ferrero <felipe.soares@brasilparalelo.com.br>
2022-12-05 09:24:48 -0300

    feat: day 5

---
+++ advent_of_code_2022/day_5.py
@@ -0,0 +1,41 @@
+from copy import deepcopy
+
+with open("advent_of_code_2022/data/day_5_input.txt", "r") as file:
+    data = file.read()
+stacks, moves = data.split("\n\n")
+
+stacks = stacks.split("\n")
+stacks, columns = stacks[:-1], stacks[-1]
+
+stacks = [
+    [row[columns.index(col)] for row in stacks if row[columns.index(col)] != " "]
+    for col in columns.strip().split()
+    if col
+]
+
+moves = [
+    [int(num) for num in command.split() if num.isnumeric()]
+    for command in moves.split("\n")
+]
+
+# Puzzle 1
+
+stacks_move = deepcopy(stacks)
+for qtty, src, dest in moves:
+    for _ in range(qtty):
+        stacks_move[dest - 1] = [stacks_move[src - 1].pop(0)] + stacks_move[dest - 1]
+
+result_1 = "".join([col[0] for col in stacks_move])
+
+print(f"Puzzle 1: {result_1}")
+
+# Puzzle 2
+
+stacks_move = deepcopy(stacks)
+for qtty, src, dest in moves:
+    stacks_move[dest - 1] = stacks_move[src - 1][:qtty] + stacks_move[dest - 1]
+    stacks_move[src - 1] = stacks_move[src - 1][qtty:]
+
+result_2 = "".join([col[0] for col in stacks_move])
+
+print(f"Puzzle 2: {result_2}")
