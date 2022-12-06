ac1ffe8c3a4eb553a69b07836a5e298723db2f13
Felipe Ferrero <felipe.soares@brasilparalelo.com.br>
2022-12-06 09:38:06 -0300

    feat: day 6

---
+++ advent_of_code_2022/day_6.py
@@ -0,0 +1,23 @@
+with open("advent_of_code_2022/data/day_6_input.txt", "r") as file:
+    data = file.read()
+
+
+def get_markers(input, len_marker):
+    blocks = [
+        (input[chr_pos - len_marker : chr_pos], chr_pos)
+        for chr_pos in range(len_marker, len(input) + 1)
+    ]
+    return filter(lambda block: len(set(block[0])) == len_marker, blocks)
+
+
+# Puzzle 1
+
+result_1 = next(get_markers(data, 4))[1]
+
+print(f"Puzzle 1: {result_1}")
+
+# Puzzle 2
+
+result_2 = next(get_markers(data, 14))[1]
+
+print(f"Puzzle 1: {result_2}")
